91cec0c7221542ff569534070c5ffc35950df00e
rdoneplus <rdoneplus@gmail.com>
2020-10-27 13:28:39 +0200

    הוצאת מידע מאתר 'משק כישורית' ושמירתו בקובץ אקסל + הוצאת מידע מאתר 'סולטן' ושמירתו בקובץ אקסל Date 27/10/2020

---
+++ ScrapePage.py
@@ -0,0 +1,94 @@
+from bs4 import BeautifulSoup
+import requests
+from Vegetable import Vegetable
+
+
+"""
+scrapePageKishurit - This method scrape a web page and extract the relevant data (price and name ) to a list in 
+"משק כישורית" website.
+ """
+
+
+def scrapePageKishurit(pageNumber,pageLink):
+    """
+    :rtype list of vegetables.
+    :param pageNumber: the page number in this webpage.
+    :param pageLink: the web page address
+    :return Vegetables: all the vegetables products in this page.
+    """
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+               "Accept-Encoding": "gzip, deflate",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
+               "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+    r = requests.get(
+        pageLink + str(pageNumber),
+        headers=headers)  # , proxies=proxies)
+    content = r.content
+    soup = BeautifulSoup(content,features="html.parser")
+
+    Vegetables = []
+    for Element in soup.findAll('div', attrs={'class': 'layout_list_item css_class_47954'}):
+        temp = Element.find('a').contents[0]
+        index = temp.find('(')
+        # retrieve the unit
+        unit = temp[index:]
+        # retrieve the name
+        name = temp[:index]
+        # retrieve the price
+        price = Element.find('strong').contents[0]
+        price = price.replace('₪', '')
+        Vegetables.append(Vegetable(name, price, unit))
+
+    return Vegetables
+
+
+
+"""scrapePageSultan - This method scrape a web page and extract the relevant data (price and name ) to a list in 
+"סולטן" website. """
+def scrapePageSultan(pageLink):
+    """
+      :rtype list of vegetables.
+      :param pageLink: the web page address
+      :return Vegetables: all the vegetables products in this page.
+    """
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+               "Accept-Encoding": "gzip, deflate",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
+               "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+    r = requests.get(pageLink,headers=headers)  # , proxies=proxies)
+    content = r.content
+    soup = BeautifulSoup(content, features="html.parser")
+    Vegetables = []
+
+    for Element,ElementPrice in zip(soup.findAll('td', attrs={'class': 'col-name'}),soup.findAll('td', attrs={'class': 'col-price'})):
+        temp = Element.contents[0]
+        # retrieve unit
+        if 'קילו' in temp:
+            index = temp.find('קילו')
+        elif 'צרור' in temp:
+            index = temp.find('צרור')
+        elif 'יחידה' in temp:
+            index = temp.find('יחידה')
+        elif 'ליטר וחצי' in temp:
+            index = temp.find('ליטר וחצי')
+        else:
+           index = -1
+        if index == -1:
+            unit = 'לא מצויין'
+            # retrieve name and delete duplicate spaces
+            name = " ".join(temp.split())
+        else:
+            unit = temp[index:]
+            unit = " ".join(unit.split())
+            # retrieve name
+            name = temp[:index]
+            name = " ".join(name.split())
+        # retrieve price and delete duplicate spaces
+        price = ElementPrice.contents[0]
+        price = " ".join(price.split())
+        Vegetables.append(Vegetable(name, price, unit))
+    return Vegetables
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+"""
+First attempt to scrape.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Web page link:
+ 1. http://www.meshek-kishorit.org/47955-%D7%99%D7%A8%D7%A7%D7%95%D7%AA
+ 2. http://sultan.pricecall.co.il/
 
+Helpfull sites:
+1. https://www.dataquest.io/blog/web-scraping-tutorial-python/
+2. https://www.youtube.com/watch?v=ng2o98k983k
+3. https://www.udemy.com/course/web-scraping-python-bs/learn/lecture/13378628#overview
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+Trying to scrape couple of fruits/vegetables names and prices, into a Array.
+"""
+from ScrapePage import scrapePageSultan
+from ScrapePage import scrapePageKishurit
+import pandas as pd
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Link to 'משק כישורית' site.
+    KishuritLink = 'http://www.meshek-kishorit.org/47955-%D7%99%D7%A8%D7%A7%D7%95%D7%AA?page='
+    SultanLink = 'http://sultan.pricecall.co.il/'
+    pageCount = 3  # The number of pages in "משק כישורית" webpage.
+
+    Vegetables = []  # All the vegetables from "משק כישורית" webpage and "סולטן" webpage.
+    Data = []  # One list of all the vegetables
+    for pageIndex in range(1,pageCount + 1):
+        Vegetables.append(scrapePageKishurit(pageIndex, KishuritLink))
+
+    # falttern the data to single list
+    Data = [item for sublist in Vegetables for item in sublist]
+    # convert the data to a table
+    df = pd.DataFrame.from_records([d.to_dist() for d in Data])
+    # create a cvs file out of the table
+    df.to_csv('משק כישורית.csv', index=False, encoding='utf-8')
+    df = pd.read_csv("משק כישורית.csv")
+    temp = pd.ExcelWriter('משק כישורית.xlsx')
+    # create a excel flie from cvs
+    df.to_excel(temp, index=False)
+    temp.save()
+
+    # Create a excel file for sultan site too
+    Vegetables = scrapePageSultan(SultanLink)
+    df = pd.DataFrame.from_records([v.to_dist() for v in Vegetables])
+    df.to_csv('סולטן.csv', index=False, encoding='utf-8')
+    df = pd.read_csv('סולטן.csv')
+    temp = pd.ExcelWriter('סולטן.xlsx')
+    df.to_excel(temp, index=False)
+    temp.save()
