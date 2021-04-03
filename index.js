8873e4f56dfe7beef9ff372d58f724660f8a9877
rdoneplus <rdoneplus@gmail.com>
2021-04-03 17:08:51 +0300

    הצגת טבלה בדף html

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const sql = require('mssql');
+
+
+const app = express()
+app.use(express.static('public/'));
+const config = {
+        server: 'LAPTOP-VNSLHC31',  //update me
+        user: 'Remez',
+        password: '123456789',
+        database: "BraudeProject",
+        dialect: "mssql",
+        port: 1433,
+        dialectOptions: {
+        instanceName: "SQLEXPRESS"
+        },
+        options:{
+            encrypt: false
+        }
+    };
+const conn = new sql.ConnectionPool(config);
+
+
+app.get('/data', (req, res) =>{
+    var record = conn.connect( function (err){
+        if (err)
+            throw err;
+        var req = new sql.Request(conn);
+        req._query('SELECT * FROM AllProds', function (err, recordSet){
+           if (err) throw err;
+           else
+           {
+               conn.close();
+               // console.dir(recordSet[0])
+               res.json(recordSet[0]);
+           }
+
+        });
+    })
+});
+
+app.listen(3001,() => console.log('App Running'))
---
+++ public/src/app.js
@@ -0,0 +1,46 @@
+{
+    async function updateTable(root){
+        const table = root.querySelector(".content-table");
+        const response = await fetch(root.dataset.url);
+        const headers = ['שם מוצר', 'יחידה', 'מחיר', 'אתר', 'מוצר בסיסי'];
+        const data = await response.json();
+        for(let val of data) {
+            console.log(val);
+        }
+        // Clear Table
+        table.querySelector("thead tr").innerHTML = '';
+        table.querySelector("tbody").innerHTML = '';
+        for (const header of headers){
+            table.querySelector('thead tr').insertAdjacentHTML('afterbegin', `<th>${ header }</th>`);
+        }
+        for (const row of data){
+            // console.log(row.Prod_Name);
+            table.querySelector('tbody').insertAdjacentHTML('beforebegin', `
+            <tr>
+            <td>${ row.Base_Prod }</td>
+            <td>${ row.Prod_Web }</td>
+            <td>${ row.Prod_Price }</td>
+            <td>${ row.Prod_Unit }</td>
+            <td>${ row.Prod_Name }</td>
+            </tr>
+        `);
+        }
+    }
+    for (const root of document.querySelectorAll('.table-refresh[data-url]')){
+        const table = document.createElement('table');
+        table.classList.add('content-table')
+        table.innerHTML = `
+            <thead>
+                <tr></tr>
+            </thead>
+            
+            <tbody>
+                <tr>
+                    <td>Loading</td>
+                </tr>  
+            </tbody>
+        `;
+        root.append(table);
+        updateTable(root);
+    }
+}
